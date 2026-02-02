f0b6e073b4f28e2c30f0f4289641ddda9ee7d9ad
arayzendev <alexandre.lin@viacesi.fr>
2026-02-02 13:29:57 +0100

    Ajout des Models

---
+++ Consoleview/Models/BackupType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleView.Models
+{
+    internal class BackupType
+    {
+        public string FullBackup { get; set; }
+        public string IncrementialBackup { get; set; }
+    }
+}
