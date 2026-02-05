d675a3e5c6981dfe338004e6599b524786bfa015
SliiZ <julien.petitcolboc@gmail.com>
2026-02-05 12:10:19 +0100

    feat: add managers

---
+++ EasySave/Managers/ConfigManager.cs
@@ -0,0 +1,39 @@
+using System.Text.Json;
+
+class ConfigManager
+{
+    private string filePath;
+
+    public ConfigManager()
+    {
+        string easySaveFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "EasySave");
+
+        if (!Directory.Exists(easySaveFolder))
+        {
+            Directory.CreateDirectory(easySaveFolder);
+        }
+
+        this.filePath = Path.Combine(easySaveFolder, "config.json");
+    }
+    public List<BackupJob> Load()
+    {
+        if (!File.Exists(filePath))
+        {
+            return new List<BackupJob>();
+        }
+
+        string json = File.ReadAllText(filePath);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return new List<BackupJob>();
+        }
+
+        List<BackupJob>? backupJobs = JsonSerializer.Deserialize<List<BackupJob>>(json);
+        return backupJobs ?? new List<BackupJob>();
+    }
+    public void Save(List<BackupJob> backupJobs)
+    {
+        File.WriteAllText(filePath, JsonSerializer.Serialize(backupJobs, new JsonSerializerOptions { WriteIndented = true }));
+    }
+}
