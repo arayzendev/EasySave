c93976d5b8ad6dce59cd5b25bd6d39fee82ec9a5
alexdev <alexandre.lin@viacesi.fr>
2026-02-06 10:36:02 +0100

    Feature/logger (#5)
    * Ajout du model, Interface, strategy et dll log
    
    * Ajout de Log et Factory
    
    * Ajout de Log, fullbackup et diffbackup

---
+++ EasyLog/LogEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLog
+{
+    public class LogEntry
+    {
+        public DateTime? Timestamp;
+        public string Application;
+        public Dictionary<string, object> data;
+    }
+}
