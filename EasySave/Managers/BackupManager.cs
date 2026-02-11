9d2a70dab8a825e312be2e6310baf373aa8a48e6
SliiZ <julien.petitcolboc@gmail.com>
2026-02-11 14:27:40 +0100

    fix: correct namespaces and encoding issues
    - Add missing namespace declarations (EasySave.Managers, EasySave.Models)
    - Fix encoding issues with special characters (accents)
    - Remove unused packages.config from EasyLog
    - Update using statements to resolve compilation errors

--- EasySave/Managers/BackupManager.cs
+++ EasySave/Managers/BackupManager.cs
@@ -11,135 +11,138 @@ using System.Diagnostics;
 using System.IO;
 using System.Text.Json;
 
-class BackupManager {
-
-    //Attributs paramï¿½tre des sauvegardes
-    private Config config;
-    private StateManager stateManager;
-    private ConfigManager configManager;
-    private BackupStrategyFactory backupStrategyFactory;
-    private Logger logger;
-
-    /// <summary>
-    /// Constructeur
-    /// </summary>
-    public BackupManager()
+namespace EasySave.Managers
+{
+    class BackupManager
     {
-        configManager = new ConfigManager();
-        stateManager = new StateManager();
-        backupStrategyFactory = new BackupStrategyFactory();
-        config = configManager.Load();
-        LanguageManager.Instance.SetLanguage(config.language.ToString());
-        InitializeLogger();
-    }
+        //Attributs paramètre des sauvegardes
+        private Config config;
+        private StateManager stateManager;
+        private ConfigManager configManager;
+        private BackupStrategyFactory backupStrategyFactory;
+        private Logger logger;
 
-    private void InitializeLogger()
-    {
-        string logDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EasySaveData", "Logs");
-        ILogFormatter formatter = LogFormatterFactory.Create(config.logType.ToString());
-        logger = new Logger(logDirectory, formatter);
-    }
-
-    public void SetLanguage(string language)
-    {
-        switch (language.ToLower())
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        public BackupManager()
         {
-            case "fr":
-                config.language=Language.FR;
-                break;
-            default:
-                config.language=Language.EN;
-                break;
-        }
-        LanguageManager.Instance.SetLanguage(language);
-        configManager.Save(config);
-    }
-
-    public void SetLog(string logType)
-    {
-        switch (logType.ToLower())
-        {
-            case "xml":
-                config.logType=LogType.XML;
-                break;
-            default:
-                config.logType=LogType.JSON;
-                break;
-        }
-        configManager.Save(config);
-        InitializeLogger();
-    }
-
-    /// <summary>
-    /// Crï¿½ation d'un travailleur de sauvegarde
-    /// </summary>
-    /// <param name="name"></param>
-    /// <param name="sourcePath"></param>
-    /// <param name="targetPath"></param>
-    /// <param name="backupStrategy"></param>
-    /// <returns></returns>
-    public bool CreateJob(string name, string sourcePath, string targetPath, string backupStrategy)
-    {   
-        //Vï¿½rifie si on dï¿½passe pas les 5 travailleurs
-        if (config.backupJobs.Count >= 5)
-        {
-            return false;
+            configManager = new ConfigManager();
+            stateManager = new StateManager();
+            backupStrategyFactory = new BackupStrategyFactory();
+            config = configManager.Load();
+            LanguageManager.Instance.SetLanguage(config.language.ToString());
+            InitializeLogger();
         }
 
-        //Crï¿½ation du travailleur
-        IBackupStrategy strategy = backupStrategyFactory.Create(backupStrategy);
-        config.backupJobs.Add(new BackupJob(name,sourcePath,targetPath,strategy,backupStrategy));
+        private void InitializeLogger()
+        {
+            string logDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EasySaveData", "Logs");
+            ILogFormatter formatter = LogFormatterFactory.Create(config.logType.ToString());
+            logger = new Logger(logDirectory, formatter);
+        }
 
-        //Sauvegarde de la configuration du travailleur
-        configManager.Save(config);
-        return true;
-    }
+        public void SetLanguage(string language)
+        {
+            switch (language.ToLower())
+            {
+                case "fr":
+                    config.language = Language.FR;
+                    break;
+                default:
+                    config.language = Language.EN;
+                    break;
+            }
+            LanguageManager.Instance.SetLanguage(language);
+            configManager.Save(config);
+        }
 
-    /// <summary>
-    /// Suppresion d'un travailleur de sauvegarde
-    /// </summary>
-    /// <param name="index"></param>
-    public void DeleteJob(int index)
-    {
-        config.backupJobs.RemoveAt(index);
-        configManager.Save(config);
-    }
+        public void SetLog(string logType)
+        {
+            switch (logType.ToLower())
+            {
+                case "xml":
+                    config.logType = LogType.XML;
+                    break;
+                default:
+                    config.logType = LogType.JSON;
+                    break;
+            }
+            configManager.Save(config);
+            InitializeLogger();
+        }
 
-    /// <summary>
-    /// Modifie ses chemins
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="sourcePath"></param>
-    /// <param name="targetPath"></param>
-    public void ModifyJob(int index, string sourcePath, string targetPath)
-    {
-        config.backupJobs[index].UpdatePaths(sourcePath,targetPath);
-        configManager.Save(config);
-    }
+        /// <summary>
+        /// Création d'un travailleur de sauvegarde
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="sourcePath"></param>
+        /// <param name="targetPath"></param>
+        /// <param name="backupStrategy"></param>
+        /// <returns></returns>
+        public bool CreateJob(string name, string sourcePath, string targetPath, string backupStrategy)
+        {
+            //Vérifie si on dépasse pas les 5 travailleurs
+            if (config.backupJobs.Count >= 5)
+            {
+                return false;
+            }
 
-    /// <summary>
-    /// Choix du travailleur ï¿½ executer
-    /// </summary>
-    /// <param name="index"></param>
-    public void ExecuteJob(int index)
-    {
-        config.backupJobs[index].Execute(OnProgressUpdate, logger);
-    }
+            //Création du travailleur
+            IBackupStrategy strategy = backupStrategyFactory.Create(backupStrategy);
+            config.backupJobs.Add(new BackupJob(name, sourcePath, targetPath, strategy, backupStrategy));
 
-    /// <summary>
-    /// Liste les travailleurs existants
-    /// </summary>
-    /// <returns></returns>
-    public List<BackupJob> ListJobs()
-    {
-        return config.backupJobs;
-    }
+            //Sauvegarde de la configuration du travailleur
+            configManager.Save(config);
+            return true;
+        }
 
-    /// <summary>
-    /// Mise ï¿½ jour de l'ï¿½tat du travailleur
-    /// </summary>
-    private void OnProgressUpdate()
-    {
-        stateManager.Write(config.backupJobs);   
+        /// <summary>
+        /// Suppresion d'un travailleur de sauvegarde
+        /// </summary>
+        /// <param name="index"></param>
+        public void DeleteJob(int index)
+        {
+            config.backupJobs.RemoveAt(index);
+            configManager.Save(config);
+        }
+
+        /// <summary>
+        /// Modifie ses chemins
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="sourcePath"></param>
+        /// <param name="targetPath"></param>
+        public void ModifyJob(int index, string sourcePath, string targetPath)
+        {
+            config.backupJobs[index].UpdatePaths(sourcePath, targetPath);
+            configManager.Save(config);
+        }
+
+        /// <summary>
+        /// Choix du travailleur à executer
+        /// </summary>
+        /// <param name="index"></param>
+        public void ExecuteJob(int index)
+        {
+            config.backupJobs[index].Execute(OnProgressUpdate, logger);
+        }
+
+        /// <summary>
+        /// Liste les travailleurs existants
+        /// </summary>
+        /// <returns></returns>
+        public List<BackupJob> ListJobs()
+        {
+            return config.backupJobs;
+        }
+
+        /// <summary>
+        /// Mise à jour de l'état du travailleur
+        /// </summary>
+        private void OnProgressUpdate()
+        {
+            stateManager.Write(config.backupJobs);
+        }
     }
 }
