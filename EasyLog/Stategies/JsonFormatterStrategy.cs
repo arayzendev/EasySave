e3a21fa832c0aa93460db28b19e0103e4e0f1319
Julien Petit--Colboc <101480069+SliiZ-code@users.noreply.github.com>
2026-02-11 14:19:44 +0100

    Master into Dev (#22)
    * Dev (#16) v1.1
    
    * Ajout du model, Interface, strategy et dll log
    
    * feat: add BackupManager (#3)
    
    * feat: add BackupManager
    
    * feat: add managers
    
    * feat: add BackupJob
    
    * fix: structure
    
    * Fix/diagrams restructuration (#4)
    
    * feat: add BackupManager
    
    * feat: add managers
    
    * feat: add BackupJob
    
    * fix: structure
    
    * feat: add constructors in class diagram
    
    * diagrammeclass
    
    * ajout séquence activation
    
    ---------
    
    Co-authored-by: arayzendev <alexandre.lin@viacesi.fr>
    
    * Feature/logger (#6)
    
    * Ajout de Log et Factory
    
    * Ajout de Log, fullbackup et diffbackup
    
    * réajustement
    
    * fix: import Json library (#7)
    
    * Ajout Langue Manager et Program
    
    * Ajout LanguageManager et Program.cs
    
    * Installation de Package json dans easysave
    
    * ajout json dans log
    
    * suppression package numerics et json
    
    * easylog net10 (#8)
    
    Co-authored-by: Julien Petit--Colboc <101480069+SliiZ-code@users.noreply.github.com>
    
    * Fix/package (#9)
    
    * easylog net10
    
    * fix: EasyLog.csproj
    
    ---------
    
    Co-authored-by: arayzendev <alexandre.lin@viacesi.fr>
    
    * Modification Program pour le paramètre strategy, modif BackupManager de la méthode create pour executer la stratégie, net 5 à 10
    
    * feat: implement v1.0 (#11)
    
    * feat: implement v1.0
    
    * Réajustement des langues
    
    ---------
    
    Co-authored-by: arayzendev <alexandre.lin@viacesi.fr>
    
    * feat: add manuals examples (#12)
    
    * Commentaire sur toute la solution
    
    * Feature/14 ajouter le choix de format de log avec xml (#15)
    
    * feat: new Config model storing jobs, language and logType
    
    * feat: store language at user choice
    
    * fix: store config correctly
    
    * feat: add logType choice
    
    * feat : add xml support
    
    ---------
    
    Co-authored-by: Julien Petit--Colboc <101480069+SliiZ-code@users.noreply.github.com>
    Co-authored-by: GONDOUE KPAN MICHEE <kpanmichee.gondoue@viacesi.fr>
    
    * Dev (#20)
    
    * Ajout du model, Interface, strategy et dll log
    
    * feat: add BackupManager (#3)
    
    * feat: add BackupManager
    
    * feat: add managers
    
    * feat: add BackupJob
    
    * fix: structure
    
    * Fix/diagrams restructuration (#4)
    
    * feat: add BackupManager
    
    * feat: add managers
    
    * feat: add BackupJob
    
    * fix: structure
    
    * feat: add constructors in class diagram
    
    * diagrammeclass
    
    * ajout séquence activation
    
    ---------
    
    Co-authored-by: arayzendev <alexandre.lin@viacesi.fr>
    
    * Feature/logger (#6)
    
    * Ajout de Log et Factory
    
    * Ajout de Log, fullbackup et diffbackup
    
    * réajustement
    
    * fix: import Json library (#7)
    
    * Ajout Langue Manager et Program
    
    * Ajout LanguageManager et Program.cs
    
    * Installation de Package json dans easysave
    
    * ajout json dans log
    
    * suppression package numerics et json
    
    * easylog net10 (#8)
    
    Co-authored-by: Julien Petit--Colboc <101480069+SliiZ-code@users.noreply.github.com>
    
    * Fix/package (#9)
    
    * easylog net10
    
    * fix: EasyLog.csproj
    
    ---------
    
    Co-authored-by: arayzendev <alexandre.lin@viacesi.fr>
    
    * Modification Program pour le paramètre strategy, modif BackupManager de la méthode create pour executer la stratégie, net 5 à 10
    
    * feat: implement v1.0 (#11)
    
    * feat: implement v1.0
    
    * Réajustement des langues
    
    ---------
    
    Co-authored-by: arayzendev <alexandre.lin@viacesi.fr>
    
    * feat: add manuals examples (#12)
    
    * Commentaire sur toute la solution
    
    * Feature/14 ajouter le choix de format de log avec xml (#15)
    
    * feat: new Config model storing jobs, language and logType
    
    * feat: store language at user choice
    
    * fix: store config correctly
    
    * feat: add logType choice
    
    * feat : add xml support
    
    * fix: structure (#19)
    
    ---------
    
    Co-authored-by: arayzendev <alexandre.lin@viacesi.fr>
    Co-authored-by: GONDOUE KPAN MICHEE <kpanmichee.gondoue@viacesi.fr>
    
    * Réorganisation logique
    
    ---------
    
    Co-authored-by: alexdev <alexandre.lin@viacesi.fr>
    Co-authored-by: GONDOUE KPAN MICHEE <kpanmichee.gondoue@viacesi.fr>

---
+++ EasyLog/Stategies/JsonFormatterStrategy.cs
@@ -0,0 +1,15 @@
+using EasyLog.Interfaces;
+using Newtonsoft.Json;
+
+namespace EasyLog
+{
+    public class JsonFormatterStrategy : ILogFormatter
+    {
+        public string FileExtension => "json";
+
+        public string Format(LogEntry entry)
+        {
+            return JsonConvert.SerializeObject(entry, Formatting.Indented);
+        }
+    }
+}
